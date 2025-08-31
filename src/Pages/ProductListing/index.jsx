2f1d96ef6ad8078e6823ad1a86f660542ed2dcaa
Thejaskrishna <cryptonic2003@gmail.com>
2025-08-31 23:39:11 +0530

    update

--- src/Pages/ProductListing/index.jsx
+++ src/Pages/ProductListing/index.jsx
@@ -99,7 +99,7 @@ const ProductListing = () => {
         )}
         
         <div className="container flex gap-3">
-          <div className={`sidebarWrapper fixed -bottom-[100%] left-0 w-fulllg:h-full lg:static lg:w-[20%] bg-white z-[102] lg:z-[100] p-3 lg:p-0  transition-all lg:opacity-100 opacity-0 ${context?.openFilter === true ? 'open' : ''}`}>
+          <div className={`sidebarWrapper fixed -bottom-[100%] left-0 w-full lg:h-full lg:static lg:w-[20%] bg-white z-[50] lg:z-[100] p-3 lg:p-0 transition-all lg:opacity-100 opacity-0 ${context?.openFilter === true ? 'open' : ''}`}>
             <Sidebar
               productsData={productsData}
               setProductsData={setProductsData}
@@ -112,7 +112,7 @@ const ProductListing = () => {
 
           {
             context?.windowWidth < 992 &&
-            <div className={`filter_overlay w-full h-full bg-[rgba(0,0,0,0.5)] fixed top-0 left-0 z-[101]  ${context?.openFilter === true ? 'block' : 'hidden'}`}
+            <div className={`filter_overlay w-full h-full bg-[rgba(0,0,0,0.5)] fixed top-0 left-0 z-[40] ${context?.openFilter === true ? 'block' : 'hidden'}`}
               onClick={()=>context?.setOpenFilter(false)}
             ></div>
           }
