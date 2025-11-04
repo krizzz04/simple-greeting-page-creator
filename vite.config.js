14634661b69a186d7720f782f1c4b7a8f5b4c3a0
Thejaskrishna <cryptonic2003@gmail.com>
2025-11-04 23:08:52 +0530

    production vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://tara-g1nf.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
